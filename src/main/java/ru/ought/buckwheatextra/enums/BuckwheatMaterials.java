39df8150cad34dfceaf0c73f2423627a6339fe87
StragaSevera <ought@yandex.ru>
2019-01-07 13:33:17 +0300

    Add different coated steels

--- src/main/java/ru/ought/buckwheatextra/enums/BuckwheatMaterials.java
+++ src/main/java/ru/ought/buckwheatextra/enums/BuckwheatMaterials.java
@@ -1,6 +1,5 @@
 package ru.ought.buckwheatextra.enums;
 
-import gregtech.GT_Mod;
 import gregtech.api.enums.Dyes;
 import gregtech.api.enums.MaterialBuilder;
 import gregtech.api.enums.Materials;
@@ -10,39 +9,81 @@ import gregtech.api.enums.TextureSet;
 import gregtech.api.interfaces.IMaterialHandler;
 import gregtech.api.objects.MaterialStack;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraftforge.fluids.FluidRegistry;
-import ru.ought.buckwheatextra.BuckwheatExtra;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BuckwheatMaterials implements IMaterialHandler {
     public static Materials CadmiumSolution;
+    public static Materials CadmiumTitaniumSolution;
     public static Materials CadmiumCoatedSteel;
+    public static Materials CadmiumCoatedBlackSteel;
+    public static Materials CadmiumCoatedRedSteel;
+    public static Materials CadmiumTitaniumCoatedRedSteel;
 
     @Override
     public void onMaterialsInit() {
-        CadmiumSolution = new MaterialBuilder(380, TextureSet.SET_FLUID, "Cadmium Solution")
+        CadmiumSolution = new MaterialBuilder(379, TextureSet.SET_FLUID, "Cadmium Solution")
                 .setRGB(48, 47, 47).setColor(Dyes.dyeBlack).addCell().addFluid()
                 .setMaterialList(new MaterialStack(Materials.Cadmium, 1))
                 .constructMaterial();
-        CadmiumCoatedSteel = new MaterialBuilder(381, TextureSet.SET_METALLIC, "Cadmium Сoated " +
-                "Steel").setRGB(48, 47, 47).setColor(Dyes.dyeBlack)
-                .addMetalItems().addToolHeadItems().addGearItems()
-                .setToolSpeed(6.0f).setDurability(512)
-                .setMaterialList(new MaterialStack(Materials.Cadmium, 1),
-                        new MaterialStack(Materials.Steel, 1))
+        CadmiumTitaniumSolution = new MaterialBuilder(380, TextureSet.SET_FLUID, "Cadmium-Titanium Solution")
+                .setRGB(80, 47, 80).setColor(Dyes.dyePurple).addCell().addFluid()
+                .setMaterialList(new MaterialStack(Materials.Cadmium, 9), new MaterialStack(Materials.Titanium, 1))
                 .constructMaterial();
-        CadmiumCoatedSteel.remove(SubTag.SMELTING_TO_FLUID);
-        CadmiumCoatedSteel.setEnchantmentForTools(Enchantment.silkTouch, 1);
+
+        CadmiumCoatedSteel = createCoatedMetal(381, CadmiumSolution, 1, Materials.Steel, 1, 60, 60, 60, Dyes.dyeBlack);
+        CadmiumCoatedBlackSteel = createCoatedMetal(382, CadmiumSolution, 1, Materials.BlackSteel, 1, 20, 20, 20,
+                Dyes.dyeBlack);
+        CadmiumCoatedRedSteel = createCoatedMetal(383, CadmiumSolution, 1, Materials.RedSteel, 1, 60, 20, 20,
+                Dyes.dyeRed);
+        CadmiumTitaniumCoatedRedSteel = createCoatedMetal(384, CadmiumTitaniumSolution, 1, Materials.RedSteel, 9, 80,
+                30, 80, Dyes.dyePurple);
+
+    }
+
+    private List<Materials> disableOrePrefixesList = new ArrayList<>();
+
+    private Materials createCoatedMetal(int subID, Materials coating, int coatingAmount, Materials metal,
+                                        int metalAmount, int r, int g, int b, Dyes dye) {
+        return createCoatedMetal(subID, coating, coatingAmount, metal, metalAmount, r, g, b, dye, Enchantment.silkTouch, 1);
+    }
+
+    private Materials createCoatedMetal(int subID, Materials coating, int coatingAmount, Materials metal,
+                                        int metalAmount, int r, int g, int b, Dyes dye, Enchantment enchantment, int enchantmentLevel) {
+        String name = getCoatedMetalName(coating, metal);
+        Materials coatedMetal = new MaterialBuilder(subID, TextureSet.SET_METALLIC, name)
+                .setRGB(r, g, b).setColor(dye)
+                .addMetalItems().addToolHeadItems().addGearItems()
+                .setToolSpeed(metal.mToolSpeed).setDurability(metal.mDurability)
+                .setMaterialList(new MaterialStack(coating, coatingAmount),
+                        new MaterialStack(metal, metalAmount))
+                .constructMaterial();
+        coatedMetal.remove(SubTag.SMELTING_TO_FLUID);
+        coatedMetal.setEnchantmentForTools(enchantment, enchantmentLevel);
+        disableOrePrefixesList.add(coatedMetal);
+        return coatedMetal;
+    }
+
+    private String getCoatedMetalName(Materials coating, Materials metal) {
+        return coating.mDefaultLocalName.split(" ")[0] + " Coated " + metal.mDefaultLocalName;
     }
 
     @Override
     public void onComponentInit() {
-        OrePrefixes.dust.disableComponent(CadmiumCoatedSteel);
-        OrePrefixes.dustSmall.disableComponent(CadmiumCoatedSteel);
-        OrePrefixes.dustTiny.disableComponent(CadmiumCoatedSteel);
-        OrePrefixes.nugget.disableComponent(CadmiumCoatedSteel);
-        OrePrefixes.plateDouble.disableComponent(CadmiumCoatedSteel);
-        OrePrefixes.screw.disableComponent(CadmiumCoatedSteel);
+        for (Materials material : disableOrePrefixesList) {
+            disableMetalOrePrefixes(material);
+        }
+    }
+
+    private void disableMetalOrePrefixes(Materials metal) {
+        OrePrefixes.dust.disableComponent(metal);
+        OrePrefixes.dustSmall.disableComponent(metal);
+        OrePrefixes.dustTiny.disableComponent(metal);
+        OrePrefixes.nugget.disableComponent(metal);
+        OrePrefixes.plateDouble.disableComponent(metal);
+        OrePrefixes.screw.disableComponent(metal);
     }
 
     @Override
